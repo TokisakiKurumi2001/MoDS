36243f40122b437577cecd068713ce5a7d84d009
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2024-06-24 10:43:26 +0700

    Diversity select sample

--- diverse-data-selection/run.py
+++ diverse-data-selection/run.py
@@ -1,37 +1,35 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '6'
-
 import json
-import sys
 import numpy as np
-from transformers import BertTokenizer, BertModel,AutoModel
+from transformers import AutoTokenizer, AutoModel
 import torch
 from kcenter_greedy import *
+from tqdm import tqdm
+import argparse
+from loguru import logger
 
+MAX_PROMPT_LENGTH=860
 
 @torch.no_grad()
 def bert_embedding(texts,batch=100):
-    tokenizer = BertTokenizer.from_pretrained('../models/bert-base-uncased')
-    model = AutoModel.from_pretrained('../models/bert-base-uncased').cuda()
-    # 将文本转化为BERT模型可识别的token序列
-    encoded_texts = tokenizer(texts,return_tensors="pt",truncation=True,padding=True,max_length=96)
-    encoded_texts =  encoded_texts.to("cuda")
+    tokenizer = AutoTokenizer.from_pretrained('../model_ckpt/bert-base-uncased')
+    model = AutoModel.from_pretrained('../model_ckpt/bert-base-uncased')
+    device = torch.device('cuda:0')
+    model = model.to(device)
     cls_hid_li = []
-    # 使用BERT模型对每个文本序列进行编码,提取其语义向量
-    i= 0
-    while i < len(texts):
-        last_hids = model(input_ids=encoded_texts["input_ids"][i:i+batch],
-                          attention_mask=encoded_texts["attention_mask"][i:i+batch])['last_hidden_state']
-        cls_hids = last_hids[:,0,:].squeeze()
+    for i in tqdm(range(0, len(texts), batch), desc="Encoding", unit='batch'):
+        batch_sample = texts[i:i+batch]
+        inputs = tokenizer(batch_sample, return_tensors='pt', truncation=True, padding=True, max_length=MAX_PROMPT_LENGTH)
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+        last_hids = model(**inputs).last_hidden_state
+        cls_hids = last_hids[:, 0, :].squeeze()
         cls_hid_li.append(cls_hids)
-        i+= batch
-        print(i)
-    # 将所有文本的embedding连成特征矩阵
+        i += batch
+    
     cls_hids_tensor = torch.concat(cls_hid_li, dim=0)
     np.save("bert_embedding.npy",cls_hids_tensor.cpu())
     return np.array(cls_hids_tensor.cpu())
 
-# 数据采样
 def sample_func(text_list,K):
     result = []
     if os.path.exists("bert_embedding.npy"):
@@ -45,32 +43,35 @@ def sample_func(text_list,K):
     k_center = kCenterGreedy(text_embedding)
     
     already_selected = None
-    #for _ in range(K):
     result = k_center.select_batch_(text_embedding,already_selected,K)
-        #result = result + new_data
-        #already_selected += new_data
     return result
 
-
 def main(input_file, output_file, K):
-    data = json.load(fp=open(input_file, "r"))
+    data = []
+    with open(input_file) as fin:
+        for line in fin:
+            _data = json.loads(line)
+            data.append(_data)
+
+    logger.info(f'There are {len(data)} samples in {input_file}')
+
     instruction_list = []
     for d in data:
-        instruction_list.append(d["instruction"])
+        instruction_list.append(d["prompt"])
     res = sample_func(text_list = instruction_list, K = K)
-    print('data length')
-    print(len(data))
-    
-    print('sampling data:')
-    print(len(res))
-    print(res)
+    logger.success(f"Successfully sample {len(res)}.")
+
     data_li = []
     for index in res:
         data_li.append(data[index])
-    json.dump(obj=data_li,fp=open(output_file,"w"),indent=2,ensure_ascii=False)
+    with open(output_file, 'w+') as fout:
+        for d in data_li:
+            fout.write(json.dumps(d) + "\n")
 
 if __name__ == "__main__":
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-    K = int(sys.argv[3])
-    main(input_file, output_file, K)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', type=str)
+    parser.add_argument('--output_file', type=str)
+    parser.add_argument('--top-k', type=int)
+    args = parser.parse_args()
+    main(args.input_file, args.output_file, args.top_k)
