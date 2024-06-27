893224ef7d307912e0054cef8fdb4fa603f549b0
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2024-06-27 16:20:00 +0700

    Merge aware of duplication

--- necessity-evaluation/merge.py
+++ necessity-evaluation/merge.py
@@ -13,16 +13,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     input_list = []
+    hash_ls = []
     with open(args.input_1) as fin:
         for line in fin:
             _data = json.loads(line)
+            # since there may be overlapped, a hash function is use to filter out duplicate
+            hash_ls.append(hash(_data['prompt']))
             input_list.append(_data)
 
+    num_duplicates = 0
     with open(args.input_2) as fin:
         for line in fin:
             _data = json.loads(line)
-            input_list.append(_data)
+            line_hash = hash(_data['prompt'])
+            if line_hash in hash_ls:
+                num_duplicates += 1
+                continue
+            else:
+                input_list.append(_data)
 
+    logger.info(f'Number of duplicated iterms: {num_duplicates}')
     logger.info(f'Number of from both input files: {len(input_list)}')
 
     with open(args.output_file, 'w+') as fout:
